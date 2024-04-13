fcbdd3210ed306b7b57334650dde70a6fd678df7
Eddy <eddydiaz0219@gmail.com>
2024-04-13 16:13:23 -0500

    add copy keyboard functionality

--- client/src/PreviewBox/PreviewBox.tsx
+++ client/src/PreviewBox/PreviewBox.tsx
@@ -4,11 +4,13 @@ interface PreviewBoxProps {
 
 export default function PreviewBox({ boxShadow }: PreviewBoxProps) {
     return (
+        <>
         <div
-        className="bg-[#158cba] w-[400px] h-[250px] mx-auto overflow-visible border-0"
-        style={{ boxShadow }}
+            className="bg-[#158cba] w-[400px] h-[250px] mx-auto overflow-visible border-0"
+            style={{ boxShadow }}
         >
         </div>
+        </>
     );
 }
   
